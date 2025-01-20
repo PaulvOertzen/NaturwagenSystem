d50609d3950388b3ef5200e3aa1d32035525ddab
Paul v. Oertzen <76756239+Nitro-paul@users.noreply.github.com>
2025-01-20 14:29:03 +0100

    Update connectEspToWifiByHiemke.ino

--- misc/connectEspToWifiByHiemke.ino
+++ misc/connectEspToWifiByHiemke.ino
@@ -2,22 +2,25 @@
 const char* ssid = "hiemke"; 
 const char* password = "gehscheissen";
 
+void ConnectToWifi() {
+    int nCntCnter = 0;
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.println("Connecting to WiFi..");
+        nCntCnter = nCntCnter + 1;
+        if ( nCntCnter > 10 ) {
+            ESP.restart();
+        }
+    }
+    Serial.println(WiFi.status());
+    Serial.println(WiFi.localIP());
+    Serial.println("Connected...");
+}
 void setup() {
     // put your setup code here, to run once:
   Serial.begin(115200); 
-  WiFi.begin(ssid, password);
-  int nCntCnter = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println("Connecting to WiFi..");
-    nCntCnter = nCntCnter + 1;
-    if ( nCntCnter > 10 ) {
-      ESP.restart();
-    }
-  }
-   Serial.println(WiFi.status());
-    Serial.println(WiFi.localIP());
-  Serial.println("Connected...");
+  WiFi.begin(ssid, password)
+  ConnectToWifi();
 }
 
 void loop() {
@@ -25,6 +28,7 @@ void loop() {
     // For disconnected Mesage when wifi disconnected
     if (WiFi.status() != WL_CONNECTED) {
         Serial.println("WiFi Disconnected...")
+        ConnectToWifi();
     }
   
   // put your main code here, to run repeatedly:
